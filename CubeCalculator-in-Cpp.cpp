61b5939b28d43afd4d5f5b396fe8b7b77a7f62a7
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-02-22 16:08:45 +0500

    Initial commit

---
+++ CubeCalculator-in-Cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num;
+    cout << "Enter a number to get cube : ";
+    cin >> num;
+
+    cout << "The cube of the entered number is : " << num * num * num;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CubeCalculator-in-Cpp
+ This simple C++ program calculates the cube of a number entered by the user.
